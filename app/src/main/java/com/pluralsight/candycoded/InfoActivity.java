70d3e60b0f9dc2b9866f5fec94424c503b65194e
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-11-20 09:23:50 +0000

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,4 +45,11 @@ public class InfoActivity extends AppCompatActivity {
         }
 
     }
+
+    public void createPhoneIntent(View view) {
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 }
